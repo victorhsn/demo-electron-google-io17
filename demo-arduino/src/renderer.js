357392b4fa4c221e76b3896468c158eecca99e2c
Victor Hugo <hugo.hsn@gmail.com>
2017-05-19 23:11:34 -0400

    electron + pulse + johnny-five

--- demo-arduino/src/renderer.js
+++ demo-arduino/src/renderer.js
@@ -26,9 +26,9 @@ process.__defineGetter__('stdin', function() {
 let board = new five.Board();
 let btnOn = document.getElementById('btnOn');
 let btnOff = document.getElementById('btnOff');
-let imagestatus = document.getElementById('img-status');
 
-btnOff.disabled = true;
+
+
 document.body.style.backgroundColor = '#000000';
 
 board.on('ready', function() {
@@ -41,25 +41,9 @@ board.on('ready', function() {
 
     });
 
-    
-    /*var led = new five.Led(13);
-
-    btnOn.addEventListener('click', () => {
-  
-      led.on();
-      
-      btnOn.disabled = true;
-      btnOff.disabled = false;
-      imagestatus.style.backgroundImage = "url('img/light-bulb.jpg')"
-
-   });
-
    btnOff.addEventListener('click', () => {
      
-      led.off();
-      btnOn.disabled = false;
-      btnOff.disabled = true;
-      imagestatus.style.backgroundImage = "url('img/ghost-pacman.png')";
+      array.stop().off();
   
-   })*/
+   })
 });
